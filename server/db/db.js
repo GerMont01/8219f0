fefb04ef36d3e0fc850155a5d26c3baf6f0df2d5
Mworekwa Ezekiel <vanheaven6@gmail.com>
2022-02-23 21:13:14 -0800

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,8 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize({
-  host:'localhost',
-  port:5432,
-  database:'messenger',
-  dialect:'postgres',
-  username:'postgres',
-  password:'admin'
-});
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+  logging: false
+})
 
 module.exports = db;
 
